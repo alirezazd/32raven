d59015d438dd146d3897108ad69c45de5f882198
Alireza <alirezazd@gmail.com>
2026-01-19 12:26:55 -0400

    feat: Integrate MAVLink parsing and forwarding for EP2 UART, update UART configurations, and switch console to USB_SERIAL_JTAG.

--- esp32/main/user_config.hpp
+++ esp32/main/user_config.hpp
@@ -4,7 +4,7 @@
 
 // STM32 UART Link Configuration
 constexpr Uart::Config kStm32UartConfig = {
-    .uart_num = 1,
+    .uart_num = 0,
     .tx_gpio = GPIO_NUM_4,
     .rx_gpio = GPIO_NUM_5,
     .baud_rate = 115200, // Console
@@ -15,10 +15,10 @@ constexpr Uart::Config kStm32UartConfig = {
 
 // EP2 Receiver UART Configuration
 constexpr Uart::Config kEp2UartConfig = {
-    .uart_num = 0,
-    .tx_gpio = GPIO_NUM_20, // Verify with schematic
-    .rx_gpio = GPIO_NUM_21, // Verify with schematic
-    .baud_rate = 115200,    // Configurable?
+    .uart_num = 1,
+    .tx_gpio = GPIO_NUM_20,
+    .rx_gpio = GPIO_NUM_21,
+    .baud_rate = 460800, // Fixed ELRS MAVLink rate
     .parity = Uart::Config::Parity::kNone,
     .rx_buf = 2048,
     .tx_buf = 2048,
