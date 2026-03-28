4d471e56bba90e3edef7225279b890bf270e4739
Alireza <alirezazd@gmail.com>
2026-03-28 19:35:25 -0300

    feat: add Wi-Fi access point configuration and validation

--- esp32/drivers/wifi.hpp
+++ esp32/drivers/wifi.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "esp_netif_types.h"
+#include "esp_wifi_types.h"
 
 class WifiController {
 public:
@@ -9,10 +10,21 @@ public:
     return instance;
   }
   struct Config {
-    const char *ssid = "32Raven";
-    const char *pass = "32Raven@1234";
-    int channel = 1;
-    int max_conn = 1;
+    struct AccessPoint {
+      const char *ssid = nullptr;
+      const char *password = "";
+      uint8_t channel = 1;
+      uint8_t max_connections = 1;
+      uint16_t beacon_interval_tu = 100;
+      bool hidden = false;
+    } ap;
+
+    struct ProtectedManagementFrames {
+      bool capable = true;
+      bool required = false;
+    } pmf;
+
+    wifi_ps_type_t power_save = WIFI_PS_NONE;
   };
 
   bool StartAp();
