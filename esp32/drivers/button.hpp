5b8beb14b6e5875ebde4c80397ca5e1ac276bb3c
Alireza <alirezazd@gmail.com>
2026-04-06 18:47:33 -0300

    feat: refactor Button class to improve state management and event handling

--- esp32/drivers/button.hpp
+++ esp32/drivers/button.hpp
@@ -38,22 +38,29 @@ class Button {
   void FlushEvents();
 
  private:
+  enum class Phase : uint8_t {
+    kReleased,
+    kDebouncingPress,
+    kPressed,
+    kDebouncingRelease,
+  };
+
   friend class System;
   void Init(const Config &cfg);
   bool ReadRawPressed() const;
+  void EnterPressed(TimeMs now_ms);
+  void EnterReleased(TimeMs now_ms);
+  void UpdateHoldEvents(TimeMs now_ms);
   // config
   gpio_num_t pin_ = GPIO_NUM_NC;
   bool active_low_ = true;
   TimeMs debounce_ms_ = 30;
   TimeMs long_press_ms_ = 500;
   TimeMs long_long_press_ms_ = 1500;
-  // debouncer
-  bool raw_last_ = false;
-  bool stable_ = false;
-  TimeMs raw_last_change_ms_ = 0;
-  // state
-  bool pressed_ = false;
-  TimeMs press_start_ms_ = 0;
+  Phase phase_ = Phase::kReleased;
+  bool raw_pressed_ = false;
+  TimeMs debounce_started_ms_ = 0;
+  TimeMs press_started_ms_ = 0;
   bool long_fired_ = false;
   bool long_long_fired_ = false;
   // event latches
