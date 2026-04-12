21857576d46df2af5e3d3bc31f1016493ffbbb69
Alireza <alirezazd@gmail.com>
2026-04-11 22:42:16 -0300

    feat: [feat:esp32] enhance FcLink with packet queuing

--- esp32/services/fc_link.hpp
+++ esp32/services/fc_link.hpp
@@ -4,15 +4,12 @@
 #include <cstdint>
 #include <optional>
 
-#include "mavlink.hpp"
 #include "message.hpp"
-#include "timebase.hpp"
 #include "uart.hpp"
 
 class FcLink {
  public:
   struct Config {
-    uint8_t rc_forward_rate_hz = 0;
     uint16_t handshake_attempts = 0;
     uint16_t handshake_retry_period_ms = 0;
     uint8_t invalid_packet_threshold = 0;
@@ -23,10 +20,12 @@ class FcLink {
     return instance;
   }
   void Poll();
-  void ForwardRcState(const RcState &rc_state);
   void PerformHandshake();  // Blocking handshake (Ping/Pong)
+  void SendPacket(const message::Packet &pkt);
 
   std::optional<message::Packet> PopPacket();
+  size_t PendingRxPacketCount() const;
+  void QueueRxPacket(const message::Packet &pkt);
 
  private:
   friend class System;
@@ -41,7 +40,6 @@ class FcLink {
   Config cfg_;
   static constexpr size_t kMaxRxReadBufferSize =
       message::kMaxPayload + message::kPacketOverhead;
-  TimeMs next_rc_forward_ms_ = 0;
 
   enum class RxState { kMagic1, kMagic2, kId, kLen, kPayload, kCrc1, kCrc2 };
   RxState rx_state_ = RxState::kMagic1;
@@ -54,5 +52,4 @@ class FcLink {
     uint8_t payload[message::kMaxPayload];
     uint16_t crc;
   } rx_pkt_internal_;
-  void SendPacket(const message::Packet &pkt);
 };
